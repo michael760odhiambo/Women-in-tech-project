ed507ba6be164cc12e4d0a4eeaa0551f2fbe5ec2
michael <mikelkarije@gmail.com>
2019-09-26 18:13:02 +0300

    save changes to my web app and necessary changes

--- app/main/views.py
+++ app/main/views.py
@@ -10,6 +10,7 @@ def home():
     #all_posts = Post.query.all()
     #print(all_posts)
     return render_template('base.html')
+    
 
 @main.route('/user/<uname>')
 @login_required
@@ -76,3 +77,25 @@ def update_pic(uname):
         #user_photo = PhotoProfile(pic_path = path,user = user)
         db.session.commit()
     return redirect(url_for('main.profile',uname=uname,id_user=current_user.id))
+
+@main.route('/motive')
+def motive():
+    return"""
+    <div class="container-fluid" style="background-color:rgb(219, 168, 219) " text-align='center'>
+    <dive class="row">
+    
+    <h1 style="background-color:rgb(219, 168, 219) " text-align='center'>Hello welcome to women in tech world motivations!</h1>
+
+    <iframe src="https://www.youtube.com/embed/NkL5_IA-ZiA" width="440" height="200" frameborder="0" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/fOJ-KKtzDXI" width="440" height="200" frameborder="0" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/WBdNfOJ5vMY" width="440" height="200" frameborder="0" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/XzQjpLB350s" width="440" height="200" frameborder="0" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/kvefEAtrla0" width="440" height="200" frameborder="0" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/8sMOSMKNYKY" width="440" height="200" frameborder="0" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/8VZTtRX4HIk" width="440" height="200" frameborder="0" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/KHq_EDi2PE8" width="440" height="200" frameborder="0" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/BYP5NZVqB50" width="440" height="200" frameborder="0" allowfullscreen></iframe>
+
+    </dive>
+</div>
+"""
--- virtual/bin/activate.csh
+++ virtual/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/moringa/Documents/flask-project/WOMENINTECH/virtual"
+setenv VIRTUAL_ENV "/home/michael/Desktop/project/Women-in-tech-project/virtual"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
