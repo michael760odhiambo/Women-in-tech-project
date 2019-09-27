eee2ecafcea496b4d25cc759a438a10f1682cca9
lavylipesh <lilibethlangat@yahoo.com>
2019-09-26 22:33:48 -0400

    import ligin_manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,30 @@
 
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app(config_state):
+    app = Flask(__name__)
+    app.config.from_object(config_options[config_state])
+
+
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    app.config['SECRET_KEY'] = 'c78ae99b1d1bf96a160fcb108bfad5d7'
+
+    return app
