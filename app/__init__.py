2bdd17b4425675849f4ee429d969cb24fc8b621c
lavylipesh <lilibethlangat@yahoo.com>
2019-09-26 22:34:48 -0400

    import secrets

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
 
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+import secrets
 
 
 login_manager = LoginManager()
